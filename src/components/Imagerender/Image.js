24cda1dbb89991717cb5bb4675d5ccefe1cc5553
mahak-appsdeployer <mahak@appsdeployer.com>
2023-04-01 11:09:30 +0530

    desired changes made

--- src/components/Imagerender/Image.js
+++ src/components/Imagerender/Image.js
@@ -7,36 +7,53 @@ const Image = () => {
   const [articles, setarticle] = useState([]);
   const [totalResult, settotalResult] = useState(0);
   const [img, setImg] = useState("");
+  const [page, setpage] = useState(1);
 
   const getImage = (image) => {
     setImg(image);
-  }
+  };
 
   const fetchMoreData = async () => {
-    let url = `https://pixabay.com/api/?key=34747400-f1e2efd17448c375ffc346561&q=${img}&per_page=20&image_type=photo`;
+    let url = `https://pixabay.com/api/?key=34747400-f1e2efd17448c375ffc346561&q=${img}&page=${page + 1}&per_page=20&image_type=photo`;
+    setpage(page + 1);
     let data = await fetch(url);
     let parseddata = await data.json();
     setarticle(articles.concat(parseddata.hits));
     settotalResult(parseddata.totalHits);
-
   };
 
+  const updateData = async () => {
+     let url = `https://pixabay.com/api/?key=34747400-f1e2efd17448c375ffc346561&q=${img}&page=${page}&per_page=20&image_type=photo`;
+     let data = await fetch(url);
+     let parseddata = await data.json();
+     setarticle(articles.concat(parseddata.hits));
+    settotalResult(parseddata.totalHits);
+  }
+
   useEffect(() => {
-    fetchMoreData();
+    updateData();
     //eslint-disable-next-line
   }, [img]);
 
   return (
     <>
-      <Navbar getImage={ getImage} />
+      <Navbar getImage={getImage} />
       <div className="mt-5"></div>
-      {articles ? (
-        <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length !== totalResult}
-          loader={<h4 className="text-center" style={{justifyContent: 'center', alignItems: 'center'}}>Loading...</h4>}
-        >
+
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResult}
+        loader={
+          <h4
+            className="text-center"
+            style={{ justifyContent: "center", alignItems: "center" }}
+          >
+            Loading...
+          </h4>
+        }
+      >
+        {articles ? (
           <div className="container-fluid">
             <div className="row">
               {articles
@@ -63,12 +80,12 @@ const Image = () => {
                 })}
             </div>
           </div>
-        </InfiniteScroll>
-      ) : (
-        <div class="text-center mt-3">
-          <span>No Data Available for the search keyword</span>
-        </div>
-      )}
+        ) : (
+          <div class="text-center mt-3">
+            <span>No Data Available for the search keyword</span>
+          </div>
+        )}
+      </InfiniteScroll>
     </>
   );
 };
